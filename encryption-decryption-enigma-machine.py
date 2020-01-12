29bc6bdbfef4153c36bf9e579f29a768a9636374
Bruno Bernardes da Costa <b.b.da.costa@gmail.com>
2020-01-12 16:19:17 -0300

    Object Oriented way

--- encryption-decryption-enigma-machine.py
+++ encryption-decryption-enigma-machine.py
@@ -1,5 +1,89 @@
 import sys
 
+
+class AbstractOperation:
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    def __init__(self, incremental_number, rotor_list):
+        self.incremental_number = incremental_number
+        self.rotor_list = rotor_list
+        
+    def calculate_position(self, character, offset):
+        pos = self.alphabet.index(character)
+        new_pos = pos + offset
+        
+        if abs(new_pos) < len(self.alphabet):
+            return new_pos
+        elif new_pos < 0:
+            times = abs(int(new_pos / len(self.alphabet)))
+            return new_pos + len(self.alphabet) * times
+        elif new_pos > 0:
+            times = abs(int(new_pos / len(self.alphabet)))
+            return new_pos - len(self.alphabet) * times
+        
+    def map_incremental_number(self):
+        pass
+    
+    def map_rotor(self):
+        pass
+    
+    
+class EncodeOperation(AbstractOperation):
+    def map_incremental_number(self, message):
+        encoded_message = ""
+        for i in range(len(message)):
+            character = message[i]
+            alphabet_pos = self.calculate_position(character, self.incremental_number + i)
+            encoded_message += self.alphabet[alphabet_pos]
+        return encoded_message
+
+    def map_rotor(self, message):
+        encoded_message = message
+        for rotor in rotor_list:
+            rotor_message = ""
+            for i in range(len(encoded_message)):
+                character = encoded_message[i]
+                alphabet_pos = self.alphabet.index(character)
+                rotor_message += rotor[alphabet_pos]
+            encoded_message = rotor_message
+        return encoded_message
+    
+
+class DecodeOperation(AbstractOperation):
+    def map_incremental_number(self, message):
+        encoded_message = ""
+        for i in range(len(message)):
+            character = message[i]
+            alphabet_pos = self.calculate_position(character, -(self.incremental_number + i))
+            encoded_message += self.alphabet[alphabet_pos]
+        return encoded_message
+    
+    def map_rotor(self, message):
+        decoded_message = message
+        for rotor in reversed(rotor_list):
+            temp_message = ""
+            for i in range(len(decoded_message)):
+                character = decoded_message[i]
+                rotor_pos = rotor.index(character)
+                temp_message += self.alphabet[rotor_pos]
+            decoded_message = temp_message
+        return decoded_message
+
+
+class EnigmaMessage:
+    def __init__(self, message):
+        self.message = message
+        
+    def encode(self, operation: EncodeOperation):
+        encoded_message = operation.map_incremental_number(self.message)
+        encoded_message = operation.map_rotor(encoded_message)
+        return encoded_message
+    
+    def decode(self, operation: DecodeOperation):
+        decoded_message = operation.map_rotor(self.message)
+        decoded_message = operation.map_incremental_number(decoded_message)
+        return decoded_message
+
 # Input
 operation = input()
 pseudo_random_number = int(input())
@@ -10,53 +94,12 @@ for i in range(3):
     
 message = input()
 
-def calculate_position(alphabet, character, offset):
-    pos = alphabet.index(character)
-    new_pos = pos + offset
-    
-    if abs(new_pos) < len(alphabet):
-        return new_pos
-    elif new_pos < 0:
-        times = abs(int(new_pos / len(alphabet)))
-        return new_pos + len(alphabet) * times
-    elif new_pos > 0:
-        times = abs(int(new_pos / len(alphabet)))
-        return new_pos - len(alphabet) * times
-
-
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-if operation == "ENCODE":
-    encoded_message = ""
-    for i in range(len(message)):
-        character = message[i]
-        alphabet_pos = calculate_position(alphabet, character, pseudo_random_number + i) 
-        encoded_message += alphabet[alphabet_pos]
-        
-    for rotor in rotor_list:
-        rotor_message = ""
-        for i in range(len(encoded_message)):
-            character = encoded_message[i]
-            alphabet_pos = alphabet.index(character)
-            rotor_message += rotor[alphabet_pos]
-        encoded_message = rotor_message
-        
-    print(encoded_message)
-
-elif operation == "DECODE":
-    rotor_message = message
-    for rotor in reversed(rotor_list):
-        temp_message = ""
-        for i in range(len(rotor_message)):
-            character = rotor_message[i]
-            rotor_pos = rotor.index(character)
-            temp_message += alphabet[rotor_pos]
-        rotor_message = temp_message
-
-    decoded_message = ""
-    for i in range(len(rotor_message)):
-        character = rotor_message[i]
-        alphabet_pos = calculate_position(alphabet, character, -(pseudo_random_number + i)) 
-        decoded_message += alphabet[alphabet_pos]
-        
+if operation == "DECODE":
+    operation = DecodeOperation(pseudo_random_number, rotor_list)
+    decoded_message = EnigmaMessage(message).decode(operation)
     print(decoded_message)
+elif operation == "ENCODE":
+    operation = EncodeOperation(pseudo_random_number, rotor_list)
+    encoded_message = EnigmaMessage(message).encode(operation)
+    print(encoded_message)
+    
